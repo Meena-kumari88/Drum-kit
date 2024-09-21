a713bf6fcbd93ce45b313573cf8c6886b97c430e
Meena Kumari <meenasahu8827@gmail.com>
2024-09-21 10:46:04 +0530

    added some css properties

--- script.js
+++ script.js
@@ -1,59 +1,60 @@
-let numberDrum = document.querySelectorAll(".drum").length;
+let currentAudio = null;
+let drums = document.querySelectorAll(".drum");
+let numberDrum = drums.length;
+
 for (let i = 0; i < numberDrum; i++) {
-    document.querySelectorAll(".drum")[i].
-        addEventListener("click", function () {
-            let buttonInerHtml = this.innerHtml;
+    drums[i].addEventListener("click", function () {
+        let buttonInnerHtml = this.innerHTML;
 
-            makeSound(buttonInerHtml);
-            buttonAnimation('buttonInerHtml');
-
-        })
+        stopCurrentSound();
+        makeSound(buttonInnerHtml);
+        buttonAnimation(buttonInnerHtml);
+    });
 }
 
 document.addEventListener("keypress", function (event) {
+    stopCurrentSound();
     makeSound(event.key);
     buttonAnimation(event.key);
-})
+});
 
 function makeSound(key) {
-    switch (key) {
-        case "w":
-            let tom1 = new Audio("sounds/drum1.mp3");
-            tom1.play();
-            break;
-        case "a":
-            let tom2 = new Audio("sounds/drum2.mp3");
-            tom2.play();
-            break;
-        case "s":
-            let tom3 = new Audio("sounds/drum3.mp3");
-            tom3.play();
-            break;
-        case "d":
-            let tom4 = new Audio("sounds/drum4.mp3");
-            tom4.play();
-            break;
-        case "j":
-            let tom5 = new Audio("sounds/drum5.mp3");
-            tom5.play();
-            break;
-        case "k":
-            let tom6 = new Audio("sounds/drum6.mp3");
-            tom6.play();
-            break;
-        case "l":
-            let tom7 = new Audio("sounds/drum7.mp3");
-            tom7.play();
-            break;
-        default:console.log(key)    
+    const sounds = {
+        "w": "sounds/drum1.mp3",
+        "a": "sounds/drum2.mp3",
+        "s": "sounds/drum3.mp3",
+        "d": "sounds/drum11.mp3",
+        "j": "sounds/drum10.mp3",
+        "k": "sounds/drum6.mp3",
+        "l": "sounds/drum7.mp3"
+    };
+    
+    if (sounds[key]) {
+        if (typeof Audio !== "undefined") {
+            currentAudio = new Audio(sounds[key]);
+            currentAudio.play();
+        } else {
+            console.error("Audio API is not supported.");
+        }
+    } else {
+        console.log(`Invalid key: ${key}`);
     }
 }
 
-function buttonAnimation(currentKey){
-    let activeButton = document.querySelector("."+currentKey)
-    activeButton.classList.add("pressed");
+function stopCurrentSound() {
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+    }
+}
 
-    setTimeout(function(){
-        activeButton.classList.remove("pressed")
-    },100)
+function buttonAnimation(currentKey) {
+    let activeButton = document.querySelector("." + currentKey);
+    if (activeButton) {
+        activeButton.classList.add("pressed");
+
+        setTimeout(function () {
+            activeButton.classList.remove("pressed");
+        }, 100);
+    }
 }
